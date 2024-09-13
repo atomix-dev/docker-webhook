487b148634defd35f0a8bb8ef95d8d63094da7c3
zulfikarrosadi <zulfikarrosadi31@gmail.com>
2024-09-13 10:58:35 +0700

    create docker webhook

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const rateLimit = require('express-rate-limit');
+const { exec } = require('child_process');
+
+const app = express();
+
+const limiter = rateLimit({
+  windowMs: 5 * 60 * 1000, // 5 minutes
+  max: 8,
+});
+
+app.use(express.json());
+app.use(limiter);
+
+const COMMAND = `docker rm bios-uin -f && docker pull squizy/bios-test:latest && docker compose -d --build .`;
+
+app.post('/webhook', (req, res) => {
+  console.log('webhook received');
+  const body = req.body;
+  console.log(body);
+
+  if(!body || !body.tag) {
+    console.log('no webhook payload');
+    return res.status(400).send('Invalid webhook payload')
+  }
+  if (body && body.push_data.tag === 'latest') {
+    const repositoryName = body.repository.repo_name;
+    const tag = body.push_data.tag;
+    console.log(`New image pushed: ${repositoryName}:${tag}`);
+
+    // Trigger a Kubernetes rollout restart
+    const deploymentName = 'bios'; // Update with your Kubernetes deployment name
+    exec(COMMAND, (err, stdout, stderr) => {
+      if (err) {
+        console.error(`Error restarting deployment: ${stderr}`);
+        return res.status(500).send('Failed to restart deployment');
+      }
+      console.log(`Deployment restarted successfully: ${stdout}`);
+      res.status(200).send('Deployment restarted');
+    });
+  } else {
+    res.status(400).send('Invalid webhook payload');
+  }
+});
+
+app.listen(3000, console.log('running on port 3000'));
