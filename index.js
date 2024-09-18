b2fa5099b837fc70227dc33adfe5d648fd55ac9f
atomix-dev <dev@radenintan.ac.id>
2024-09-18 13:15:42 +0700

    change webhook to github webhook

--- index.js
+++ index.js
@@ -1,49 +1,39 @@
 const express = require('express');
-const rateLimit = require('express-rate-limit');
 const { exec } = require('child_process');
-require('dotenv').config()
+const { Webhook } = require('@ocktokit/webhooks');
+require('dotenv').config();
 
 const app = express();
 
-const limiter = rateLimit({
-  windowMs: 5 * 60 * 1000, // 5 minutes
-  max: 8,
-});
-
 app.use(express.json());
-app.use(limiter);
 
-const DOCKER_PASSWORD = process.env.DOCKER_PASSWORD
+const DOCKER_PASSWORD = process.env.DOCKER_PASSWORD;
 
 const COMMAND = `echo ${DOCKER_PASSWORD} | docker login --username squizy --password-stdin && docker rm bios-uin -f && docker pull squizy/bios-test:latest && docker run -d --rm --name bios-uin -p 8000:80 --env-file ./.env squizy/bios-test:latest`;
 
-app.post('/webhook', (req, res) => {
-  console.log('webhook received');
-  const body = req.body;
-  console.log(body);
-
-  if (!body || !body.push_data) {
-    console.log('no webhook payload');
-    return res.status(400).send('Invalid webhook payload')
-  }
-  if (body && body.push_data.tag === 'latest') {
-    const repositoryName = body.repository.repo_name;
-    const tag = body.push_data.tag;
-    console.log(`New image pushed: ${repositoryName}:${tag}`);
-
-    // Trigger a Kubernetes rollout restart
-    const deploymentName = 'bios'; // Update with your Kubernetes deployment name
-    exec(COMMAND, (err, stdout, stderr) => {
-      if (err) {
-        console.error(`Error restarting deployment: ${stderr}`);
-        return res.status(500).send('Failed to restart deployment');
-      }
-      console.log(`Deployment restarted successfully: ${stdout}`);
-      res.status(200).send('Deployment restarted');
-    });
-  } else {
-    res.status(400).send('Invalid webhook payload');
-  }
+const webhook = new Webhook({
+  secret: process.env.WEBHOOK_SECRET
 });
 
-app.listen(3000, console.log('running on port 3000'));
+app.post('/webhook', async (req, res) => {
+  const timestmap = new Date();
+  console.log(`${timestmap}: webhook received`);
+
+  const signature = req.headers["x-hub-signature-256"];
+  const result = await webhook.verify(req.body, signature);
+  if (!result) {
+    console.log(`${timestmap}: invalid signature, this is signature from header ${signature}`);
+    return res.status(401).send('Invalid signature');
+  }
+
+  exec(COMMAND, (error, stdout, stderr) => {
+    if (error) {
+      console.error(`${timestmap}: Error restarting deployment: ${stderr}`);
+      return res.status(500).send('Failed to restart deployment');
+    }
+    console.log(`${timestmap}: System restart successfully: ${stdout}`)
+    return res.status(200).send('system restart successfully');
+  });
+});
+
+app.listen(3000, console.log('${new Date()}: running on port 3000'));
